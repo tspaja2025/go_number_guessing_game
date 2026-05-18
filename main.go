cebea39ed0c9e2336bb71e40715924001c925142
tspaja2025 <tspaja2025@outlook.com>
2026-05-18 08:23:14 +0300

    Update main.go
    Change to bufio

--- main.go
+++ main.go
@@ -1,31 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	min, max := 1, 100
-	source := rand.NewSource(time.Now().UnixNano())
-	randomizer := rand.New(source)
+
+	randomizer := rand.New(rand.NewSource(time.Now().UnixNano()))
 	randomNumber := randomizer.Intn(max-min) + min
 	fmt.Println("The random number is", randomNumber)
 
+	reader := bufio.NewReader(os.Stdin)
+
 	fmt.Println("Welcome to Number Guessing Game")
-	fmt.Println("Guess a number between 1 and 100")
-	fmt.Println("Please input your guess")
 
-	var guess int
+	for {
+		fmt.Println("Guess a number between 1 and 100")
+		fmt.Println("Please input your guess")
 
-	fmt.Scan(&guess)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
 
-	if guess > randomNumber {
-		fmt.Println("Your guess is bigger than the random number. Try again")
-	} else if guess < randomNumber {
-		fmt.Println("Your guess is smaller than the random number. Try again")
-	} else {
-		fmt.Println("Your guess was correct!")
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Please enter a valid number")
+			continue
+		}
+
+		if guess > randomNumber {
+			fmt.Println("Your guess is bigger than the random number. Try again")
+		} else if guess < randomNumber {
+			fmt.Println("Your guess is smaller than the random number. Try again")
+		} else {
+			fmt.Println("Your guess was correct!")
+			break
+		}
 	}
 }
